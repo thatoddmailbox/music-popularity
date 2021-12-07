84deb9cd8a5b4665bd474590db233b0ef837f516
Alex Studer <alex@studer.dev>
2021-12-06 20:38:54 -0500

    parser: filter out duplicate songs

--- parser/dataset.py
+++ parser/dataset.py
@@ -12,6 +12,7 @@ class Dataset:
 
 		self.song_index = []
 		self.songs_by_id_pad = {}
+		self.existing = set()
 		with open(self.index_file_path, newline="") as index_file:
 			index_raw_data = csv.reader(index_file)
 			first = True
@@ -24,5 +25,12 @@ class Dataset:
 				s = song.Song(self, row)
 				if s.id_pad not in self.songs_with_data:
 					continue
+				if (s.title, s.artist) in self.existing:
+					# remove duplicates
+					# (this happens when a song is on multiple charts)
+					# (we only care about the peak and the weeks, so OK to throw awat)
+					continue
+
 				self.song_index.append(s)
 				self.songs_by_id_pad[s.id_pad] = s
+				self.existing.add((s.title, s.artist))
